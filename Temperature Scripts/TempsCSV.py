de670ec97d5889cb97caa630c6e7205dfb5dccb6
BecketLab <102190724+BecketLab@users.noreply.github.com>
2022-07-20 17:50:49 -0700

    Create TempsCSV.py
    A python script that will send temperature data to the arduino over serial from a .CSV file.

---
+++ Temperature Scripts/TempsCSV.py
@@ -0,0 +1,22 @@
+import csv
+import serial
+import time
+
+#Edit these variables to your .csv file, serial device address, and interval (in seconds)
+tempFile = "scripps.csv"
+serAdd = '/dev/tty.usbmodem14101'
+interval = 60
+
+#Setting up serial connection  
+ser = serial.Serial(serAdd, 9600)
+
+with open(tempFile, "r") as file:
+    csvReader = csv.reader(file)
+    header = next(csvReader)
+    tempIndex = header.index("temp")
+    
+    for row in csvReader:
+        currTemp = row[tempIndex]
+        sendStr = str('<' + str(currTemp) + '>')
+        ser.write(sendStr.encode('utf-8'))
+        time.sleep(interval)
